98be852372f85d36123a7fbd87a2d80b94d19b77
ThaisKurmann <thais.martin.ferraz@gmail.com>
2025-04-07 20:21:11 +0200

    CreateSQLiteIfNotExists

--- muchMoneyUpgrade/Program.cs
+++ muchMoneyUpgrade/Program.cs
@@ -3,17 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace muchMoneyUpgrade
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        private const string DATABASE_FOLDER = "MuchMoney";
         [STAThread]
         static void Main()
         {
+            string MuchMoneyPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\MuchMoney";
+            bool checkPath = Directory.Exists(MuchMoneyPath);
+            if (!checkPath)
+            {
+                Directory.CreateDirectory(MuchMoneyPath);
+            }
+
+            string DataBaseSqlitePath =  Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\MuchMoney\\Database.sqlite";
+            bool checkFile = File.Exists(DataBaseSqlitePath);
+            if (!checkFile)
+            {
+                SQLiteConnection.CreateFile(DataBaseSqlitePath);
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
