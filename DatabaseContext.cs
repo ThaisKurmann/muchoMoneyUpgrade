5e9f1ab00a109d1b55f4c08d855fb9e13e6c724b
ThaisKurmann <thais.martin.ferraz@gmail.com>
2025-09-29 18:57:54 +0200

    CreatedSubCategoryTableOnDataBase

--- DatabaseContext.cs
+++ DatabaseContext.cs
@@ -7,6 +7,7 @@ namespace MuchMoneyUpgrade
     public class DatabaseContext : DbContext
     {
         public DbSet<Category> Categories { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
         public DatabaseContext(DbContextOptions<DatabaseContext> options)
          : base(options)
         {
